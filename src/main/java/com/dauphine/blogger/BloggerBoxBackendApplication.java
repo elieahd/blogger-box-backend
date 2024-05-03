2b3f96aabd884d8a3904a3f947b1aa476060f3c4
Elie Daher <eliedhr@gmail.com>
2024-05-03 10:24:07 -0400

    add documentation via Swagger

--- src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
+++ src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
@@ -1,9 +1,23 @@
 package com.dauphine.blogger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Blogger box backend",
+				description = "Blogger box server providing endpoints",
+				contact = @Contact(
+						name = "Elie",
+						email = "eliedhr@gmail.com"
+				),
+				version = "1.0.0"
+		)
+)
 public class BloggerBoxBackendApplication {
 
 	public static void main(String[] args) {
